76592b0d32c552851ae8dd7f3ed98198346b96bd
samuelitwaru <samuelitwaru@gmail.com>
2021-12-05 16:32:29 +0300

    form-control

--- bar/forms/purchase.py
+++ bar/forms/purchase.py
@@ -5,9 +5,9 @@ from ..models import Product
 
 
 class FilterPurchasesForm(forms.Form):
-	date_gte = forms.DateField(label="From", required=False, widget=forms.DateInput(format=('%Y-%m-%d'), attrs={'type':"date"}))
-	date_lte = forms.DateField(label="To", required=False, widget=forms.DateInput(format=('%Y-%m-%d'), attrs={'type':"date"}))
-	product = forms.IntegerField(label="Product", required=False,  widget=forms.Select(choices=[(0, "ANY")]))
+	date_gte = forms.DateField(label="From", required=False, widget=forms.DateInput(format=('%Y-%m-%d'), attrs={'type':"date", "class":"form-control"}))
+	date_lte = forms.DateField(label="To", required=False, widget=forms.DateInput(format=('%Y-%m-%d'), attrs={'type':"date", "class":"form-control"}))
+	product = forms.IntegerField(label="Product", required=False,  widget=forms.Select(choices=[(0, "ANY")], attrs={"class":"form-control"}))
 	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
